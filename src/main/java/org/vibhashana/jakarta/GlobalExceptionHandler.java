aeb491fae909edc155ce2450a0382ad40a29aa48
vibhashan <ranhindavibhashana@gmail.com>
2025-02-19 20:24:29 +0530

    feat: Implemented simple messaging with JMS

--- src/main/java/org/vibhashana/jakarta/GlobalExceptionHandler.java
+++ src/main/java/org/vibhashana/jakarta/GlobalExceptionHandler.java
@@ -1,18 +1,14 @@
 package org.vibhashana.jakarta;
 
-import org.vibhashana.jakarta.interceptors.Logging;
-
 import jakarta.json.Json;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
+public class GlobalExceptionHandler implements ExceptionMapper<Exception> {
 	@Override
-	public Response toResponse(Throwable e) {
+	public Response toResponse(Exception e) {
 		final var json = Json.createObjectBuilder()
 				.add("message", Response.Status.BAD_REQUEST.getReasonPhrase())
 				.add("code", Response.Status.BAD_REQUEST.getStatusCode()).add("cause", e.getMessage())
--- src/main/java/org/vibhashana/jakarta/messages/MsgListener.java
+++ src/main/java/org/vibhashana/jakarta/messages/MsgListener.java
@@ -1,31 +1,30 @@
-// package org.vibhashana.jakarta.messages;
+package org.vibhashana.jakarta.messages;
 
-// import java.util.logging.Logger;
+import java.util.logging.Logger;
 
-// import jakarta.ejb.EJB;
-// import jakarta.ejb.MessageDriven;
-// import jakarta.jms.JMSException;
-// import jakarta.jms.Message;
-// import jakarta.jms.MessageListener;
+import jakarta.ejb.EJB;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.JMSConnectionFactory;
+import jakarta.jms.JMSConnectionFactoryDefinition;
+import jakarta.jms.JMSDestinationDefinition;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
 
-// @MessageDriven(name = "TestQueue", mappedName = "jms/TestQueue"
-// // Not mandatory
-// // activationConfig = {
-// // @ActivationConfigProperty(name = "acknowledgeMode", value =
-// // "Auto-acknowledge"),
-// // @ActivationConfigProperty(name = "destinationType", value =
-// // "jakarta.jms.Queue") }
-// )
-// public class MsgListener implements MessageListener {
-// @EJB
-// private Receiver receiver;
+@MessageDriven(name = "TestQueue", mappedName = "java:app/jms/TestQueue")
+// Below config annotations will create the resources
+@JMSConnectionFactoryDefinition(name = "java:app/jms/TestFactory")
+@JMSDestinationDefinition(name = "java:app/jms/TestQueue", destinationName = "TestQueue", interfaceName = "jakarta.jms.Queue")
+public class MsgListener implements MessageListener {
+    @EJB
+    private Receiver receiver;
 
-// @Override
-// public void onMessage(Message message) {
-// try {
-// receiver.receive(message.getBody(String.class));
-// } catch (JMSException | InterruptedException e) {
-// Logger.getLogger(MsgListener.class.getName()).severe(e.getMessage());
-// }
-// }
-// }
+    @Override
+    public void onMessage(Message message) {
+        try {
+            receiver.receive(message.getBody(String.class));
+        } catch (JMSException | InterruptedException e) {
+            Logger.getLogger(MsgListener.class.getName()).severe(e.getMessage());
+        }
+    }
+}
